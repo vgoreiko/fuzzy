36544774a03463a0058b0f6fd8a6aa4f69504bb3
Vasyl Horeiko <vasyl.horeiko@signet.tv>
2023-03-08 23:30:07 +0200

    - added Dockerfile for postgres - added typeorm and connection

--- packages/todo-api/src/app/app.module.ts
+++ packages/todo-api/src/app/app.module.ts
@@ -1,11 +1,38 @@
 import { Module } from "@nestjs/common";
-
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import configuration from "../config/configuration";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+    TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => {
+          const db = configService.get('db');
+          const { type, host, port, username, password, database, entities, synchronize } = db;
+          return {
+            type,
+            host,
+            port: port,
+            username,
+            password,
+            database,
+            entities,
+            synchronize,
+            retryAttempts: 1,
+          };
+        },
+        inject: [ConfigService]
+      }
+    )
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
