94daffeb86c99ddd6fcc94ce0ee3ee3f4aa972b0
Vasyl Horeiko <vasyl.horeiko@signet.tv>
2023-04-22 12:22:39 +0300

    fixed path for migrations

--- packages/todo-api/src/config/typeorm.config.ts
+++ packages/todo-api/src/config/typeorm.config.ts
@@ -2,18 +2,32 @@ import { configuration } from "./configuration";
 import { DataSource } from 'typeorm';
 import { UserEntity } from '../app/user';
 import { TodoEntity } from '../app/todo';
+import * as path from "path";
 
-const config = configuration();
-const typeormConfig = config.db;
-const { type, host, port, username, password, database, migrations } = typeormConfig;
-console.log('typeormConfig:', typeormConfig);
-export const connectionSource = new DataSource({
-  type,
-  host,
-  port,
-  username,
-  password,
-  database,
+// TODO: uncomment when migrations will be running. For now - manual definition
+// const config = configuration();
+// const typeormConfig = config.db;
+// const { type, host, port, username, password, database, migrations } = typeormConfig;
+// console.log('typeormConfig:', typeormConfig);
+// export default new DataSource({
+//   type,
+//   host,
+//   port,
+//   username,
+//   password,
+//   database,
+//   entities: [UserEntity, TodoEntity],
+//   migrations,
+// });
+const p = path.join(__dirname, '..', 'migrations/*.ts')
+console.log('p:', p)
+export default new DataSource({
+  type: 'postgres',
+  host: 'host.docker.internal',
+  port: 5431,
+  username: 'postgres',
+  password: 'docker',
+  database: 'todoApp',
   entities: [UserEntity, TodoEntity],
-  migrations,
+  migrations: [p],
 });
